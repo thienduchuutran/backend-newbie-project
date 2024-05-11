9193df32d62de381d256c9741e0d0a647579f5d8
Duc <thienductranhuu2784@gmail.com>
2024-05-10 22:26:19 -0400

    moved the block of codes that connects database to the app to a seperate file

---
+++ src/config/database.js
@@ -0,0 +1,19 @@
+
+//have to import this first to get any variables from .env
+require('dotenv').config() 
+
+const mysql = require('mysql2')
+
+
+//test connection
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, //default was 3306
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD, //default was empty
+    database: process.env.DB_NAME,
+  });
+
+  //since we gonna use variable "connection" in server.js, we gotta export it
+  module.exports = connection
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express') //commonjs
 //import express from 'express' (es modules)
 
-const mysql = require('mysql2')
+const connection = require('./config/database')
 
 const path = require('path')
 const configViewEngine = require('./config/viewEngine')
@@ -19,21 +19,12 @@ configViewEngine(app)
 app.use('/', webRoutes) //this means that all the routes we input will be behind this first param
 
 
-//test connection
-// Create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3307, //default was 3306
-  user: 'root',
-  password: '123456', //default was empty
-  database: 'hoidanit',
-});
+
 
 connection.query(
   'SELECT * FROM Users',
   function(err, results, fields){
     console.log(">>>results: ", results) //rows returned by server
-    console.log(">>>fields: ", fields)  //extra meta data about results
   }
 )
 
