93511fae614bde7b46d75d7e8a8d754ec47e75d4
Duc <thienductranhuu2784@gmail.com>
2024-05-13 11:46:36 -0400

    getting dynamic data from form user input and saving in database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -31,8 +31,25 @@ const getHoiDanIT = (req, res) => {
 }
 
 const postCreateUser = (req, res) => {
-    console.log("check req.body: ", req.body)
-    return res.send("create new user")
+    //let email = req.body.email
+    //let name = req.body.username
+    //let city = req.body.city
+
+    //advanced 
+    let {email, name, city} = req.body
+
+    console.log("check req.body: ", email, name, city)
+    //return res.send("create new user")
+
+    connection.query(
+        `INSERT INTO Users (email, name, city)
+        VALUES (?, ?, ?)`,
+        [email, name, city],
+        function(err, result){
+            console.log(result)
+            res.send('create successful')
+        }
+    );
 }
 
 module.exports = {
